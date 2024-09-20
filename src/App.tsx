c7171f4779f47fc1ec812e14b59c7ca0b2d36f17
Nikita <146779463+nikita-nn@users.noreply.github.com>
2024-09-20 13:40:18 +0300

    fix: links to projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,36 @@
-import {projects, technologies} from "./data/data.ts";
+import { projects, technologies } from "./data/data.ts";
 
 export default function App() {
   return (
     <main>
-      <section className=" mt-0 flex flex-col gap-4">
-        <h1 className=" lg:text-8xl text-7xl">Привет!</h1>
+      <section className="mt-0 flex flex-col gap-4">
+        <h1>Привет!</h1>
         <p className={"font-xl"}>Меня зовут Никита, я веб разработчик</p>
       </section>
-        <section>
-            <h2 className={"text-4xl"}>Мой стэк:</h2>
-            <div className={"main-grid"}>
-                {technologies.map((tech) => (
-                    <img src={tech} alt="" className={"w-full logo"}/>
-                ))}
-            </div>
-        </section>
-        <section>
-            <h2 className={"text-4xl"}>Мои проекты:</h2>
-            <div className={"grid lg:grid-cols-2 grid-cols-1 gap-9"}>
-                {projects.map((project) => (
-                    <div className={"card"}>
-                        <h1 className={"text-lg"}>{project.name}</h1>
-                        <p>{project.description}</p>
-                        <p className={"font-bold"}>{project.position}</p>
-                        <a
-                            href={"https://biocon.international"}
-                            className={"underline w-full"}
-                            target={"_blank"}
-                        >
-                        {project.link}
+      <section>
+        <h2>Мой stack:</h2>
+        <div className={"main-grid"}>
+          {technologies.map((tech) => (
+            <a href={tech.link} target="_blank">
+              <img src={tech.icon} alt="" className={"w-full logo"} />
+            </a>
+          ))}
+        </div>
+      </section>
+      <section>
+        <h2>Мои проекты:</h2>
+        <div className={"projects-grid"}>
+          {projects.map((project) => (
+            <div className={"card"}>
+              <h1 className={"text-lg"}>{project.name}</h1>
+              <p>{project.description}</p>
+              <p className={"font-bold"}>{project.position}</p>
+              <a
+                href={project.link}
+                className={"underline w-full"}
+                target={"_blank"}
+              >
+                {project.link}
               </a>
             </div>
           ))}
--- src/data/data.ts
+++ src/data/data.ts
@@ -1,46 +1,54 @@
-export const technologies: string[] = [
-    "/react.svg",
-    "/vite.svg",
-    "/node.svg",
-    "/typescript.svg",
-    "/javascript.svg",
-    "/html5.svg",
+interface Technology {
+  icon: string;
+  link: string;
+}
+
+export const technologies: Technology[] = [
+  { icon: "/react.svg", link: "https://react.dev" },
+  { icon: "/vite.svg", link: "https://vitejs.dev/" },
+  { icon: "/node.svg", link: "https://nodejs.org/" },
+  { icon: "/typescript.svg", link: "https://typescriptlang.org/" },
+  {
+    icon: "/javascript.svg",
+    link: "https://developer.mozilla.org/ru/docs/Web/JavaScript",
+  },
+  { icon: "/html5.svg", link: "https://html5.org" },
 ];
 
 interface ProjectsInfo {
-    name: string;
-    description: string;
-    position: string;
-    link: string;
+  name: string;
+  description: string;
+  position: string;
+  link: string;
 }
 
 export const projects: ProjectsInfo[] = [
-    {
-        name: "LMS2.0",
-        description:
-            "LMS 2.0 — это платформа, предлагающая развитие практических навыков для профессионалов в области кибербезопасности, системного администрирования и т. д.",
-        position: "Frontend + Backend",
-        link: "https://lms.itmo.xyz/",
-    },
-    {
-        name: "BIOCON 2024",
-        description:
-            "Веб-сайт международной конференции по промышленной биотехнологии в Альметьевске (Республика Татарстан)",
-        position: "Frontend + Backend",
-        link: "https://biocon.international/",
-    },
-    {
-        name: "Онлайн магистратура ИТМО",
-        description:
-            "Онлайн-магистратура на базе Университета ИТМО совместно с Positive Technologies",
-        link: "https://mcs.itmo.ru/",
-        position: "Frontend + Backend",
-    },
-    {
-        name: "SimpleBanking",
-        description:
-            "PET проект, предоставляющий базовую функциональность онлайн-банкинга",
-        position: "Frontend + Backend",
-        link: "https://github.com/nikita-nn/SimpleBanking",
-    },
+  {
+    name: "LMS2.0",
+    description:
+      "LMS 2.0 — это платформа, предлагающая развитие практических навыков для профессионалов в области кибербезопасности, системного администрирования и т. д.",
+    position: "Frontend + Backend",
+    link: "https://lms.itmo.xyz/",
+  },
+  {
+    name: "BIOCON 2024",
+    description:
+      "Веб-сайт международной конференции по промышленной биотехнологии в Альметьевске (Республика Татарстан)",
+    position: "Frontend + Backend",
+    link: "https://biocon.international/",
+  },
+  {
+    name: "Онлайн магистратура ИТМО",
+    description:
+      "Онлайн-магистратура на базе Университета ИТМО совместно с Positive Technologies",
+    link: "https://mcs.itmo.ru/",
+    position: "Frontend + Backend",
+  },
+  {
+    name: "SimpleBanking",
+    description:
+      "PET проект, предоставляющий базовую функциональность онлайн-банкинга",
+    position: "Frontend + Backend",
+    link: "https://github.com/nikita-nn/SimpleBanking",
+  },
 ];
